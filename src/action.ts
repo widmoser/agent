ae49f664c4534ec2b2f643a41da239ffa9d05b29
Hannes Widmoser <widmoser@gmail.com>
2025-06-05 23:13:48 +0200

    Update configuration so that VC contains a bundled index.js file.

--- src/action.ts
+++ src/action.ts
@@ -1,23 +1,22 @@
+import { getInput } from '@actions/core';
 import { getOctokit } from '@actions/github';
 
-export async function createPullRequestForIssue(
-  repository: string, 
-  branch: string, 
-  apiKey: string, 
-  model: string, 
-  githubToken: string
-): Promise<void> {
-  const octokit = getOctokit(githubToken)
-  await octokit.request(`POST /repos/${repository}/pulls`, {
-    owner: 'OWNER',
-    repo: 'REPO',
-    title: 'Amazing new feature',
-    body: 'Please pull these awesome changes in!',
-    head: branch,
-    base: 'master',
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  })
-}
+const repository = getInput('repository');
+const branch = getInput('branch');
 
+const apiKey = getInput('openrouter_api_key');
+const model = getInput('model');
+const githubToken = getInput('github_token');
+const octokit = getOctokit(githubToken)
+
+await octokit.request(`POST /repos/${repository}/pulls`, {
+  owner: 'OWNER',
+  repo: 'REPO',
+  title: 'Amazing new feature',
+  body: 'Please pull these awesome changes in!',
+  head: branch,
+  base: 'master',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
